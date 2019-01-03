304a849a7b91feec33e2cf698d4f800fdeeb504a
yshen <YShen@council.nyc.gov>
2019-01-03 16:08:44 -0500

    -m

--- Channels/settings.py
+++ Channels/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!i=)_wpe8qk@!@jrnfl^&omx+^^$la$pnhr1sz7!s*#)$gczz0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Django Channels
 ASGI_APPLICATION = "Channels.routing.application"
